a974eecd114242daa865a80ba2093eebfe6bcc19
dezumka <dirk.oeckinghaus@gmail.com>
2023-12-18 14:58:55 +0100

    fix statusbar color vertical alignment

--- dwm.c
+++ dwm.c
@@ -860,7 +860,7 @@ drawstatusbar(Monitor *m, int bh, char* stext) {
 
 			text[i] = '\0';
 			w = TEXTW(text) - lrpad;
-			drw_text(drw, x, 0, w, bh, 0, text, 0);
+			drw_text(drw, x, vertpadbar / 2, w, bh - vertpadbar, 0, text, 0);
 
 			x += w;
 
