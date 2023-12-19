725b15070afcdbb9da61a2ab68f638b7db27c097
dezumka <dirk.oeckinghaus@gmail.com>
2023-12-19 15:52:37 +0100

    added wallpaper to start script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 xrandr --output DP-4 --mode 1920x1080 --primary --rate 144 --output HDMI-0 --mode 2560x1440 --rate 60 --left-of DP-4
 xset r rate 600 25 &
+feh --bg-fill ~/Pictures/linux-retro_5120x2880.jpg &
 picom &
 
 # relaunch DWM if the binary changes, otherwise bail
